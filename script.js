51ed365d7fcd19a439091f81633a90a1d5b32a9e
kusum0001 <69834578+kusum0001@users.noreply.github.com>
2022-07-24 19:49:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var password = document.getElementById("password");
+function genPassword(){
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+abcdefghijklmnopqrstuvxxyz[];',./\{}";
+    var passwordLength = 10;
+    var password ="";
+
+    for(var i = 0 ;  i<=passwordLength ; i++ ){
+    var randomNumber = Math.floor(Math.random()*chars.length);
+    password += chars.substring(randomNumber,randomNumber +1);
+
+}
+
+document.getElementById("password").value = password;
+}
+
+function copyPassword(){
+  var copyText = document.getElementById("password");
+  copyText.select();
+  copyText.setSelectionRange(0,999);
+  document.execCommand("copy");
+ 
+  
+} 
+
