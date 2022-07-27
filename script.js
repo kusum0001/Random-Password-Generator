1f62c3a135397b7e53231c2a3ddb11051f41f17d
kusum0001 <69834578+kusum0001@users.noreply.github.com>
2022-07-27 17:32:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function copyPassword(){
   copyText.select();
   copyText.setSelectionRange(0,999);
   document.execCommand("copy");
-    copyText(div.innerText().then(() => alert (Text Copied));
+    copyText.innerText=> alert (Text Copied));
  
   
 } 
